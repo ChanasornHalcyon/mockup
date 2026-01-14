bd095106083c62dfa79fdefe1c510b9261a4fb11
ChanasornHalcon@github.co.th <chanasorn@halcyon.co.th>
2026-01-14 13:41:15 +0700

    JJ login

--- pages/Customers.jsx
+++ pages/Customers.jsx
@@ -12,11 +12,11 @@ const Customers = () => {
     }).format(value);
 
   const customers = [
-    { customer: "Customer1", distributor: "FDM", country: "Thailand", totalSales: 1250000 },
-    { customer: "Customer2", distributor: "Halcyon Vietnam", country: "Vietnam", totalSales: 980000 },
-    { customer: "Customer3", distributor: "YN Engineers", country: "Thailand", totalSales: 2150000 },
-    { customer: "Customer4", distributor: "YN Engineers", country: "Thailand", totalSales: 5150000 },
-    { customer: "Customer5", distributor: "Halcyon Vietnam", country: "Vietnam", totalSales: 40000 },
+    { customerid: "001", customer: "Customer1", distributor: "FDM", country: "Thailand", type: "Dealer", totalSales: 1250000 },
+    { customerid: "002", customer: "Customer2", distributor: "Halcyon Vietnam", country: "Vietnam", type: "Dealer", totalSales: 980000 },
+    { customerid: "003", customer: "Customer3", distributor: "YN Engineers", country: "Thailand", type: "Dealer", totalSales: 2150000 },
+    { customerid: "007", customer: "Customer4", distributor: "YN Engineers", country: "Thailand", type: "End User", totalSales: 5150000 },
+    { customerid: "009", customer: "Customer5", distributor: "Halcyon Vietnam", country: "Vietnam", type: "End User", totalSales: 40000 },
   ];
 
   const handleClickCustomer = (item) => {
@@ -47,9 +47,11 @@ const Customers = () => {
               <table className="min-w-full text-sm">
                 <thead className="bg-gray-50 text-gray-600">
                   <tr>
-                    <th className="px-6 py-4 text-left">Customers</th>
+                    <th className="px-6 py-4 text-left">Customers Id</th>
+                    <th className="px-6 py-4 text-left">Customers Name</th>
                     <th className="px-6 py-4 text-left">Distributor</th>
                     <th className="px-6 py-4 text-left">Country</th>
+                    <th className="px-6 py-4 text-left">Type</th>
                     <th className="px-6 py-4 text-right">Total Sales</th>
                   </tr>
                 </thead>
@@ -60,6 +62,9 @@ const Customers = () => {
                       key={index}
                       className="border-t hover:bg-blue-50/40"
                     >
+                      <td className="px-6 py-4 text-gray-600">
+                        {item.customerid}
+                      </td>
                       <td
                         onClick={() => handleClickCustomer(item)}
                         className="px-6 py-4 font-medium text-[#0B4EA2] cursor-pointer hover:underline"
@@ -71,6 +76,9 @@ const Customers = () => {
                       </td>
                       <td className="px-6 py-4 text-gray-600">
                         {item.country}
+                      </td>
+                      <td className="px-6 py-4 text-gray-600">
+                        {item.type}
                       </td>
                       <td className="px-6 py-4 text-right font-semibold text-[#0B4EA2]">
                         {formatUSD(item.totalSales)}
